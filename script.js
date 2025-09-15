d43754e7942c9fedd32927f4fd583b802ac8fb47
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-15 21:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+	const strongs = document.querySelectorAll("strong")
+	for(let strong of strongs){
+		strong.style.color = "rgb(0, 128, 0)"
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const strongs = document.querySelectorAll("strong")
+	for(let strong of strongs){
+		strong.style.color = "rgb(0, 0, 0)"
+	}
     
 }
